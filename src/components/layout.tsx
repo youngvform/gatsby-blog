1317de8aa76ad76c020e0b17f38006440ed15602
youngvform <austinhbk@naver.com>
2020-06-15 23:28:03 +0900

    updated font and minor error fixed

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { ReactNode } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface Props {
+  children: ReactNode;
+}
+const Layout = ({ children }: Props) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,6 @@ export const query = graphql`
           }
           frontmatter {
             title
-            description
             date
           }
         }
