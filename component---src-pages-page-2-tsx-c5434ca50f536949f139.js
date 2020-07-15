a53f43450fb3675fbc42f536740775dfc9ca7120
youngvform <austinhbk@naver.com>
2020-07-15 23:14:01 +0900

    Updates

--- component---src-pages-page-2-tsx-c5434ca50f536949f139.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"+xhR":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("9Dj+"),r=a("H8eV");t.default=function(e){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2 (",e.path,")"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-tsx-c5434ca50f536949f139.js.map
---
+++ component---src-templates-post-tsx-fd5b128d13a205fc2af7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"+kg1":function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return u}));var a=n("q1tI"),r=n.n(a),l=n("9Dj+"),u="1459698608";t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("div",null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}})))}}}]);
+//# sourceMappingURL=component---src-templates-post-tsx-fd5b128d13a205fc2af7.js.map
